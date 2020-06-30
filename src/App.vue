601a91d8cce441634d0c0c164b6e861ea6ff93ae
Ferdinand Grüner <suenos@Suenoss-MBP.fritz.box>
2020-06-30 16:00:30 +0200

    added questions

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <div id="app">
-    <Modal>
-    </Modal>
-  </div>
+    <div id="app">
+        <Modal>
+            <div class="flex">
+                <div class="w-1/5 bg-gray-200 flex justify-center items-start">
+                    <img class="rounded-full w-24 p-4" src="https://www.fillmurray.com/200/200" title="avatar">
+                </div>
+                <div class="flex-grow p-4">
+                    <question :question="{
+                                title: 'Do you think Sun is really an unlimited source of energy just burning there for someone to use it\'s unlimited power?',
+                                    answers: [
+                                        {
+                                            value: 0,
+                                            title: 'no',
+                                        },
+                                        {
+                                            value: 1,
+                                            title: 'yes',
+                                        }
+                                    ]
+                                }"
+                        :followUp="{
+                                    title: 'Really?',
+                                    answers: [
+                                        {
+                                            value: 2,
+                                            title: 'no',
+                                        },
+                                        {
+                                            value: 3,
+                                            title: 'yes',
+                                        }
+                                    ],
+                                   }"
+                    />
+                </div>
+            </div>
+        </Modal>
+    </div>
 </template>
 
 <script>
 import Modal from './components/Modal.vue'
+import Question from './components/Question.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Modal
-  }
+    name: 'App',
+    components: {
+        Modal,
+        Question,
+    }
 }
 </script>
---
+++ src/components/Question.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div class="font-bold">
+            {{ question.title }}
+        </div>
+
+        <hr class="my-3">
+
+        <form @submit.prevent="showFollowUp = true">
+            <fieldset>
+                <div class="flex" :key="index" v-for="(answer, index) in question.answers">
+                    <input class="mr-3" type="radio" :id="answer.title" name="question" value="answer.value">
+                    <label :for="answer.title">{{ answer.value }}</label>
+                </div>
+            </fieldset>
+
+<!--            <div v-if="showFollowUp" class="bg-gray-200">-->
+<!--                <div class="flex" :key="index" v-for="(fu, index) in followUp">-->
+<!--                    <input class="mr-3" type="radio" :id="fu.title" name="follow_up" value="answer.value">-->
+<!--                    <label :for="fu.title">{{ fu.title }}</label>-->
+<!--                </div>-->
+<!--            </div>-->
+
+            <hr class="my-3">
+
+            <div class="flex justify-end">
+                <button class="bg-red-600 px-3 py-1 text-white hover:bg-red-800" type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Question',
+    props: {
+        question: Object,
+        followUp: Object
+    },
+    data() {
+        return {
+            showFollowUp: false
+        }
+    },
+    methods: {
+        validate() {
+            this.showFollowUp = true;
+        }
+    }
+}
+</script>
